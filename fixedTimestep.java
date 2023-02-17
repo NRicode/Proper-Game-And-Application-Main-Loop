d35b248c64a834181c699de6cce46d9ebbf487bf
Marvin E <106366188+NRicode@users.noreply.github.com>
2023-02-17 22:32:09 +0700

    Update fixedTimestep.java
    convert delta time from ms to second

--- fixedTimestep.java
+++ fixedTimestep.java
@@ -1,35 +1,34 @@
 long currentTime;
 long lastTime = System.currentTimeMillis();
-long deltaTime = 0;
+float deltaTime = 0;
 
 //60 loop/second
-long LOOP_DELAY = 1000 / 60;
+int LOOP_DELAY = 1000 / 60;
 boolean quit = false;
 
 while (!quit) {
-	
-    currentTime = System.currentTimeMillis();
-    deltaTime = currentTime - lastTime;
 
-    if (deltaTime >= LOOP_DELAY) {
-		
+    currentTime = System.currentTimeMillis();
+    deltaTime = ((float)(currentTime - lastTime))/1000; //convert delta time from ms to second
+
+    if (deltaTime * 1000 >= LOOP_DELAY) {
+
         lastTime = currentTime;
-        update(deltaTime / 1000.0);
-		render();
-		
-		
+        update(deltaTime)
+		render()
+
     }
 
     // add a delay to reduce CPU usage
-    long timeToWait = LOOP_DELAY - deltaTime;
+    int timeToWait = LOOP_DELAY - ((int)(deltaTime*1000));
     if (timeToWait > 0) {
-		
+
         try {
             Thread.sleep(timeToWait);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-		
+
     }
-	
+
 }
